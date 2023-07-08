e69b4ab4b016ee6f7ec0ea8cec381e848f999d54
Ashok Kumar Prajapat <102814093+Ashok-713@users.noreply.github.com>
2023-07-09 00:42:54 +0530

    JS

---
+++ script.js
@@ -0,0 +1,7 @@
+/*============---------- Typing Animation ============----------*/
+var typed = new Typed(".typing",{
+    strings:["","Web Designer","Web Developer","Software Engineer","Artist"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop: true
+})
